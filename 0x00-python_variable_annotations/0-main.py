5c2c5b86d5ff8490cb07766be83b88663d9fb7d7
ibrahim-shoil <ibrahimshoil50@gmail.com>
2024-10-10 13:23:15 +0300

    Commit on 2024-10-10 13:23:15

--- 0x00-python_variable_annotations/0-main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""
-This script tests the add function from the 0-add module.
-"""
-add = __import__('0-add').add
-
-print(add(1.11, 2.22) == 1.11 + 2.22)
-print(add.__annotations__)
