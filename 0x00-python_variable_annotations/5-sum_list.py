13b18bde493da27ec09d0b45724a35bbb8e0d7fd
ibrahim-shoil <ibrahimshoil50@gmail.com>
2024-10-10 14:03:38 +0300

    Commit on 2024-10-10 14:03:38

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''Module that contains a function to sum a list of floats.
+'''
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''Returns the sum of a list of floats.
+
+    Args:
+        input_list (List[float]): The list of floats to sum.
+
+    Returns:
+        float: The sum of the list of floats.
+    '''
+    return sum(input_list)
