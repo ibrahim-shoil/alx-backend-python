66694e9d54511c03c8ec4c09a772fb2bf2543d72
ibrahim-shoil <ibrahimshoil50@gmail.com>
2024-10-10 13:14:59 +0300

    Commit on 2024-10-10 13:14:59

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
-
 def add(a: float, b: float) -> float:
+    """
+    Adds two floating point numbers.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of the two numbers.
+    """
     return a + b
