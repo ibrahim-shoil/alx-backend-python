6a828d65436eab26aa0738f74ba66cbecaffcaee
ibrahim-shoil <ibrahimshoil50@gmail.com>
2024-10-10 14:11:37 +0300

    Commit on 2024-10-10 14:11:37

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""comment"""
+
+
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+    """this is a retuned list as float
+    """
+
+    return sum(mxd_list)
