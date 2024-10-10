5d04096e3ea45c36f7d499d429931049d91b300a
ibrahim-shoil <ibrahimshoil50@gmail.com>
2024-10-10 14:05:00 +0300

    Commit on 2024-10-10 14:05:00

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,6 +2,8 @@
 '''Module that contains a function to sum a list of floats.
 '''
 
+from typing import List
+
 
 def sum_list(input_list: List[float]) -> float:
     '''Returns the sum of a list of floats.
