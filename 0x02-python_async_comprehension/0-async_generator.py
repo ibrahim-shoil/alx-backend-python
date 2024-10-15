3d8e7b649b8e69047fc79b0f23e827b3817cde3e
ibrahim-shoil <ibrahimshoil50@gmail.com>
2024-10-15 23:56:56 +0300

    Commit on 2024-10-15 23:56:56

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,39 +1,14 @@
 #!/usr/bin/env python3
 """
-0-async_generator.py
-This module contains an asynchronous generator that
-yields random numbers between 0 and 10.
-The generator pauses for 1 second before yielding
-each number and loops exactly 10 times.
-Functions:
-async_generator:
-    An asynchronous generator that yields 10
-    random float values between 0 and 10,
-    with a 1-second asynchronous pause between each value.
-Usage:
------------
-You can use this asynchronous generator in other coroutines
-by looping over it with 'async for',
-allowing you to retrieve the yielded values asynchronously.
+    0-async_generator.py
 """
-
-import asyncio
+from typing import Generator
 import random
-from typing import AsyncGenerator
+import asyncio
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
-    """
-    Asynchronous generator that yields random
-    float numbers between 0 and 10.
-    The generator waits asynchronously for
-    1 second before yielding each number.
-    It yields 10 values in total.
-    Yields:
-    --------
-    float:
-        A random float number between 0 and 10.
-    """
-    for _ in range(10):
+async def async_generator() -> Generator[float, None, None]:
+    """Asynchronously wait and Yield a number"""
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
