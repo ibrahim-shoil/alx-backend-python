556b287bb68103cc63fe10285d24ef66c0ff00f4
ibrahim-shoil <ibrahimshoil50@gmail.com>
2024-10-16 00:07:35 +0300

    Commit on 2024-10-16 00:07:35

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """
-    0-async_generator.py
+    1-async_comprehension.py
 """
 from typing import List
+
 async_generator = __import__('0-async_generator').async_generator
 
 
